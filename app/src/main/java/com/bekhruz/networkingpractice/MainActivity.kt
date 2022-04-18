51c158c6c6d9da45a8fff5fae3db30f94b583b34
BekhruzDev <bekhruzabdullakhujaev@gmail.com>
2022-04-18 10:02:17 +0500

    Init

--- app/src/main/java/com/bekhruz/networkingpractice/MainActivity.kt
+++ app/src/main/java/com/bekhruz/networkingpractice/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         recyclerView = binding.photosRecyclerview
